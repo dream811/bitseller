64243eda56b06c8fa15d50fd2f3cc26500cccd4f
unknown <dragonwinter811@outlook.com>
2022-07-29 11:40:14 +0900

    good

--- app/Http/Controllers/User/CashController.php
+++ app/Http/Controllers/User/CashController.php
@@ -112,7 +112,7 @@ class CashController extends Controller
     {
         $title="구매내역";
         if ($request->ajax()) {
-            $exchanges = Trading::where('user_id', Auth::id())->where('state', 1)->orderBy('created_at', 'DESC');
+            $exchanges = Trading::where('user_id', Auth::id())->orderBy('created_at', 'DESC');
 
             return DataTables::of($exchanges)
                 ->addIndexColumn()
@@ -137,7 +137,7 @@ class CashController extends Controller
     {
         $title="배당금지급내역";
         if ($request->ajax()) {
-            $exchanges = Trading::where('user_id', Auth::id())->where('state', '==', 1)->orderBy('created_at', 'DESC');
+            $exchanges = Trading::where('user_id', Auth::id())->where('state', 1)->orderBy('created_at', 'DESC');
 
             return DataTables::of($exchanges)
                 ->addIndexColumn()
